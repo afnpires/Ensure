e9baf304afd81770869ffec68188ca4578febdfc
Alexandre Pires <afnpires@outlook.com>
2018-09-01 12:08:45 +0100

    chore: Add Microsoft.CodeAnalysis.FxCopAnalyzers to all projects

---
+++ benchmarks/Ensure.Benchmarks/GlobalSuppressions.cs
@@ -0,0 +1,15 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "BenchmarkTest", Scope = "member", Target = "~M:Ensure.Benchmarks.EnsureBenchmark.Ensure_Object")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "BenchmarkTest", Scope = "member", Target = "~M:Ensure.Benchmarks.EnsureBenchmark.Ensure_Struct")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "BenchmarkTest", Scope = "member", Target = "~M:Ensure.Benchmarks.EnsureBenchmark.EnsureNotNull_Object")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "BenchmarkTest", Scope = "member", Target = "~M:Ensure.Benchmarks.EnsureBenchmark.EnsureNotNull_Struct")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "BenchmarkTest", Scope = "member", Target = "~M:Ensure.Benchmarks.EnsureBenchmark.EnsureNotNullOrEmpty_String")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "BenchmarkTest", Scope = "member", Target = "~M:Ensure.Benchmarks.EnsureBenchmark.EnsureNotNullOrWhitespace_String")]
+
+[assembly: SuppressMessage("Design", "CA1052:Static holder types should be Static or NotInheritable", Justification = "Cannot be static do to being the class used to run benchmarks", Scope = "type", Target = "~T:Ensure.Benchmarks.EnsureBenchmark")]
---
+++ tests/Ensure.Tests/GlobalSuppressions.cs
@@ -0,0 +1,17 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Test method", Scope = "Namespace", Target = "~M:Ensure.EnsureGenericExtensionsTests.Ensure_WithFailingCondition_ThrowsArgumentException")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Test method", Scope = "member", Target = "~M:Ensure.EnsureGenericExtensionsTests.Ensure_WithMissingParameters_ThrowsArgumentNullExceptio(System.Object,System.String,System.String)")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Test method", Scope = "member", Target = "~M:Ensure.EnsureStringExtensionsTests.EnsureNotNullOrEmpty_WithInvalidString_ThrowsArgumentNullException(System.String,System.String,System.String)")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Test method", Scope = "member", Target = "~M:Ensure.EnsureStringExtensionsTests.EnsureNotNullOrEmpty_WithValidString_DoesNothing")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Test method", Scope = "member", Target = "~M:Ensure.EnsureStringExtensionsTests.EnsureNotNullOrWhitespace_WithInvalidString_ThrowsArgumentNullException(System.String,System.String,System.String)")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Test method", Scope = "member", Target = "~M:Ensure.EnsureStringExtensionsTests.EnsureNotNullOrWhitespace_WithValidString_DoesNothing")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Test method", Scope = "member", Target = "~M:Ensure.EnsureGenericExtensionsTests.EnsureNotNull_WithNullArgument_ThrowsArgumentNullException(System.String,System.String)")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Test method", Scope = "member", Target = "~M:Ensure.EnsureGenericExtensionsTests.Ensure_WithNullValidator_ThrowsArgumentNullException")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Test method", Scope = "member", Target = "~M:Ensure.EnsureGenericExtensionsTests.Ensure_WithPassingCondition_DoesNothing")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Test method", Scope = "member", Target = "~M:Ensure.EnsureGenericExtensionsTests.EnsureNotNull_WithInstanceArgument_DoesNothing")]
