a1862af4aea851c05f4f2764e56614eb09ca3466
Alexandre Pires <afnpires@outlook.com>
2018-09-01 11:25:39 +0100

    chore: Document public method exposed in the library.

--- src/Ensure/EnsureGenericExtensions.cs
+++ src/Ensure/EnsureGenericExtensions.cs
@@ -4,6 +4,22 @@
 
     public static class EnsureGenericExtensions
     {
+        /// <summary>
+        ///     Throws a <seealso cref="ArgumentNullException"/> with <paramref name="name"/> as argument,
+        /// if the <paramref name="value"/> is null.
+        ///     <para />
+        ///     If no <paramref name="name"/> is passed, the nameof <paramref name="value"/> is assumed.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">
+        ///     The value to be verified.
+        /// </param>
+        /// <param name="name">
+        ///     The name that will be passed as argument of the <see cref="ArgumentNullException"/> being thrown.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///     Throws when <paramref name="value"/> is null.
+        /// </exception>
         public static void EnsureNotNull<T>(this T value, string name = null)
             where T : class
         {
@@ -13,6 +29,25 @@
             }
         }
 
+        /// <summary>
+        ///     Throws a <seealso cref="ArgumentException"/> with <paramref name="message"/> as argument,
+        /// if the <paramref name="validator"/> function returns false.
+        /// </summary>
+        /// <param name="value">
+        ///     The value to be verified.
+        /// </param>
+        /// <param name="validator">
+        ///     The function that will perform the <paramref name="value"/> validation.
+        /// </param>
+        /// <param name="message">
+        ///     The message that will be passed as argument to the <seealso cref="ArgumentException"/> being thrown.
+        /// </param>
+        /// <exception cref="ArgumentException">
+        ///     Throws when the <paramref name="validator"/> function returns false.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        ///     Throws when <paramref name="validator"/> is null or <paramref name="message"/> is null or empty.
+        /// </exception>
         public static void Ensure<T>(this T value, Func<T, bool> validator, string message)
         {
             validator.EnsureNotNull(nameof(validator));
--- src/Ensure/EnsureStringExtensions.cs
+++ src/Ensure/EnsureStringExtensions.cs
@@ -4,6 +4,18 @@
 
     public static class EnsureStringExtensions
     {
+        /// <summary>
+        ///     Throws a <seealso cref="ArgumentNullException"/> if <paramref name="value"/> is null or empty.
+        /// </summary>
+        /// <param name="value">
+        ///     The value to be verified.
+        /// </param>
+        /// <param name="name">
+        ///     The name that will be passed as argument of the <see cref="ArgumentNullException"/> being thrown.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///     Throws when <paramref name="name"/> is null or empty.
+        /// </exception>
         public static void EnsureNotNullOrEmpty(this string value, string name = null)
         {
             if (string.IsNullOrEmpty(value))
@@ -12,6 +24,18 @@
             }
         }
 
+        /// <summary>
+        ///     Throws a <seealso cref="ArgumentNullException"/> if <paramref name="value"/> is null, empty, or whitespace.
+        /// </summary>
+        /// <param name="value">
+        ///     The value to be verified.
+        /// </param>
+        /// <param name="name">
+        ///     The name that will be passed as argument of the <see cref="ArgumentNullException"/> being thrown.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///     Throws when <paramref name="value"/> is null, empty or whitespace.
+        /// </exception>
         public static void EnsureNotNullOrWhitespace(this string value, string name = null)
         {
             if (string.IsNullOrWhiteSpace(value))
